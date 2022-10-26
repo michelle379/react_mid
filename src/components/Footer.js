a251cb7b4b697084266db95061873ee1dce3ae65
michelle379 <56913621+michelle379@users.noreply.github.com>
2022-10-26 17:15:56 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,32 @@
 import React from "react";
-import FooterList from "./footerList";
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return (
-        <div className="footer">
+        <footer className="footer">
+            <h1 className="footer-title"><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FML家事服務<br/></h1>
             <div className="container" >
-                <FooterList />
+                <nav className="row">
+                    <ul className="col-12 col-md-3 list-unstyled ">
+                        <li className="text-center">客服專線 <li className="text-center">公司信箱</li><li className="text-center">公司地址</li><li className="text-center">客服時間</li></li>
+                    </ul>
+                    <ul className="col-12 col-md-6 list-unstyled text-white">
+                        <li className="text-left ">02-6639-6688<li className="text-left">service@familyclean.com.tw</li><li className="text-left">台北市大安區和平東路二段134號</li><li className="text-left">週一~週五(不含例假日)08:30~12:00, 14:00~17:30</li></li>
+                    </ul>
+                    <ul className="col-12 col-md-1 list-unstyled">
+                        <li className="text-left">更多FML</li>
+                    </ul>
+                    <ul className="col-12 col-md-2 list-unstyled">
+                        <li className="text-left"><Link to="/"><img src="/images/Line.png" className="mx-2" width='30'></img></Link><Link to="/"><img src="/images/Facebook.png" className="mx-2" width='30'></img></Link><Link to="/"><img src="/images/Instagram.png" className="mx-2" width='30'></img></Link></li>
+                    </ul>
+                    
+
+                    <div className="container">
+                        <p className="text-center mb-0 mt-2"></p>
+                    </div>
+                </nav>
             </div>
-        </div>
+            <p className="text-center mb-0 mt-2 text-white">Ⓒ 2022. Familyclean. All Rights Reserved.</p>
+        </footer>
     );
 }
