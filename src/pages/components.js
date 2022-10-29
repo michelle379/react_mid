11c320cc09bb62886a7c6509e4b2ff2ba1c50104
michelle379 <56913621+michelle379@users.noreply.github.com>
2022-10-29 16:04:39 +0800

    Update components.js

--- src/pages/components.js
+++ src/pages/components.js
@@ -2,10 +2,19 @@ import ProductGrid_list from '../components/ProductGrid_list';
 
 import { useSelector } from "react-redux";
 import {selectProducts} from "../redux/productsSlice";
+import { useParams } from 'react-router-dom';
 
 
 function Components() {
-  const products = useSelector(selectProducts);
+  let products = useSelector(selectProducts);
+  const { categoryName } = useParams();
+  const url=categoryName || "/";
+
+  if(url !="/") {
+    products = products.filter(x => x?.category.toUpperCase() === url.toUpperCase());
+  }
+
+
   return (
     <>
         <ProductGrid_list products={products}/>
